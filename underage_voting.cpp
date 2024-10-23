327060f92e0748f66162f41469ca10dcf963bb0c
hardikdhalla <dhallahardik31@gmail.com>
2024-10-23 19:50:20 +0530

    Create underage_voting.cpp

---
+++ underage_voting.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"Enter age: ";
+    cin>>age;
+    try{
+        if(age<18){
+            throw age;
+        }
+        else{
+            cout<<"Age: "<<age<<"\nAPPROVED"<<endl;
+        }
+    }
+    catch(int a){
+        cout<<"\nERROR: Underage! ("<<age<<")"<<endl;
+    }
+}
+
+/*Output
+Enter age: 20
+Age: 20
+APPROVED
+*/
